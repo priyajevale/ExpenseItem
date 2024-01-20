b3a6dc0399d408369038b6ccc603ff706e146a6f
priyajevale <priyajevale0625@gmail.com>
2024-01-20 18:44:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+// import './App.css';
+import ExpenseItem from './components/ExpenseItem.js';
+function App() {
+  return (
+    <div> 
+      <h2>lets get started</h2>
+    <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
